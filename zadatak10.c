42bcb6061e30651b30783f659c20d814b0bc318c
jodza-edu <65362961+jodza-edu@users.noreply.github.com>
2020-05-24 14:47:35 +0200

    Create zadatak10.c

---
+++ zadatak10.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+#include "string.h"
+
+void main() {
+    
+    FILE* fp1 = fopen("recenice.txt", "r");
+    FILE* fp2 = fopen("sadrze.txt", "w");
+    
+    char recenica[300]="";
+    char rec[50];
+    
+    int m;
+    int tacno = 0;
+    
+    scanf("%d", &m);
+    printf("\n");
+
+    do {
+        fscanf(fp1, "%s", &rec);
+
+        strcat(recenica, rec);
+        strcat(recenica, " ");
+
+        for (int i = 0; i < strlen(rec); i++) {
+            if (rec[i] == '.') {
+                if (strlen(recenica) > m) { 
+                    printf("%s", recenica); //debug
+                    fprintf(fp2, "%s", recenica);
+                }
+                for (int j = 0; j < strlen(recenica); j++) {
+                    recenica[j] = 0;
+                }
+            }
+            else tacno = 1;
+        }
+    } while (!feof(fp1));
+
+    fclose(fp1);
+    fclose(fp2);
+}
