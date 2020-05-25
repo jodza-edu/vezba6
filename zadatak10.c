20047d87df7166c86a60eccea29e0240a2476635
jodza-edu <65362961+jodza-edu@users.noreply.github.com>
2020-05-25 10:36:48 +0200

    Update zadatak10.c

--- zadatak10.c
+++ zadatak10.c
@@ -2,15 +2,15 @@
 #include "string.h"
 
 void main() {
-    
+
     FILE* fp1 = fopen("recenice.txt", "r");
     FILE* fp2 = fopen("sadrze.txt", "w");
-    
-    char recenica[300]="";
+
+    char recenica[300] = "";
     char rec[50];
-    
+
     int m;
-    
+
     scanf("%d", &m);
     printf("\n");
     //recenice u fajlu razdvaja tacka "."
@@ -20,15 +20,13 @@ void main() {
         strcat(recenica, rec);
         strcat(recenica, " ");
 
-        for (int i = 0; i < strlen(rec); i++) {
-            if (rec[i] == '.') {
-                if (strlen(recenica) > m) { 
-                    printf("%s", recenica); //debug
-                    fprintf(fp2, "%s", recenica);
-                }
-                for (int j = 0; j < strlen(recenica); j++) {
-                    recenica[j] = 0;
-                }
+        if (rec[strlen(rec)-1] == '.') {
+            if (strlen(recenica) > m) {
+                printf("%s", recenica); //debug
+                fprintf(fp2, "%s", recenica);
+            }
+            for (int j = 0; j < strlen(recenica); j++) {
+                recenica[j] = 0;
             }
         }
     } while (!feof(fp1));
